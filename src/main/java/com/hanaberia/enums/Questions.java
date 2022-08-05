4c91a1eae9521f55c09e19790e0cb22a9387bd10
dhkot <dong_ha@wp.pl>
2022-08-05 18:28:17 +0200

    users password ux

--- src/main/java/com/hanaberia/enums/Questions.java
+++ src/main/java/com/hanaberia/enums/Questions.java
@@ -1,12 +1,12 @@
 package com.hanaberia.enums;
 
 public enum Questions {
-    BLACK("Kim chciał(a)byś być?"),
+    BLACK("Kim jesteś/chciał(a)byś być?"),
     BLUE("Co byś powiedział(a) sobie sprzed 5 lat?"),
     RED("Co byś zrobił(a), gdyby nie było jutra?"),
-    YELLOW("W trakcie robienia czego tracisz poczucie czasu?"),
-    GREEN("Jakie dobre nawyki chciał(a)byś w sobie pielęgnować?"),
-    PURPLE("Kim jest osoba, która najbardziej Cię inspiruje?"),
+    YELLOW("Przy czym tracisz poczucie czasu?"),
+    GREEN("Jakie dobre nawyki chciał(a)byś pielęgnować?"),
+    PURPLE("Kto najbardziej Cię inspiruje?"),
     WHITE("Gdzie na Ziemi chciał(a)byś mieszkać?"),
     ORANGE("Jak nie teraz, to kiedy?");
 
